1ac0bfccb9c436d80e361cd900138655cafbeec9
Suyash00 <suyashhandy@gmail.com>
2025-07-23 07:05:51 +0530

    Troubleshooting auth route

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,10 @@ const express = require("express");
 const { register, login } = require("../controllers/authController");
 const router = express.Router();
 
+router.get("/ping", (req, res) => {
+  res.send("Auth route works!");
+});
+
 router.post("/register", register);
 router.post("/login", login);
 
